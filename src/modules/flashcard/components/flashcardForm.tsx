81625d6a707a87282d7392c959cd8e8f5584578f
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-19 21:39:36 +0700

    feat: add init data and pending status

--- src/modules/flashcard/components/flashcardForm.tsx
+++ src/modules/flashcard/components/flashcardForm.tsx
@@ -11,21 +11,15 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { useToast } from '@/shared/hooks/useToast';
 import type { FlashcardType } from '../types/flashcard';
 import { FlashcardSchema } from '../schemas/flashcard.schema';
+import React from 'react';
 
 type FlashcardFormProps = {
   onSubmit: (data: FlashcardType) => void;
   onCancel: () => void;
   initialData?: Partial<FlashcardType>;
+  isPending?: boolean;
   isEditing?: boolean;
 };
 
@@ -33,30 +27,38 @@ const FlashcardForm = ({
   onSubmit,
   onCancel,
   initialData,
+  isPending,
   isEditing = false,
 }: FlashcardFormProps) => {
-  const { toast } = useToast();
-
   const form = useForm<FlashcardType>({
     resolver: zodResolver(FlashcardSchema),
     defaultValues: {
+      id: initialData?.id || null,
       term: initialData?.term || '',
       definition: initialData?.definition || '',
-      // language: initialData?.language || '',
-      // category: initialData?.category || '',
     },
   });
+  const { reset } = form;
+
+  React.useEffect(() => {
+    if (initialData) {
+      reset({
+        id: initialData.id || null,
+        term: initialData.term || '',
+        definition: initialData.definition || '',
+      });
+    }
+  }, [reset, initialData]);
 
   const handleSubmit = (data: FlashcardType) => {
     onSubmit(data);
-    toast('Flashcard updated!');
   };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
         <div className="flex items-center justify-between mb-4">
-          <h3 className="text-xl font-semibold text-foreground">
+          <h3 className="text-lg font-semibold text-foreground">
             {isEditing ? 'Edit Flashcard' : 'Create New Flashcard'}
           </h3>
         </div>
@@ -67,9 +69,9 @@ const FlashcardForm = ({
             name="term"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>term (Question)</FormLabel>
+                <FormLabel>Term</FormLabel>
                 <FormControl>
-                  <Input placeholder="Enter the word or phrase" {...field} className="text-base" />
+                  <Input placeholder="Enter term" {...field} className="text-base py-5" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -81,64 +83,23 @@ const FlashcardForm = ({
             name="definition"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Back (Answer)</FormLabel>
+                <FormLabel>Definition</FormLabel>
                 <FormControl>
-                  <Input placeholder="Enter the translation" {...field} className="text-base" />
+                  <Input placeholder="Enter definition" {...field} className="text-base py-5" />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-
-          {/* <FormField
-            control={form.control}
-            name="language"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Language</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select a language" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent className="bg-popover">
-                    {languages.map((lang) => (
-                      <SelectItem key={lang} value={lang}>
-                        {lang}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          /> */}
-
-          {/* <FormField
-            control={form.control}
-            name="category"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Category</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="e.g., Greetings, Food, Travel"
-                    {...field}
-                    className="text-base"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          /> */}
         </div>
 
         <div className="flex gap-3 justify-end pt-4">
           <Button type="button" variant="outline" onClick={onCancel}>
             Cancel
           </Button>
-          <Button type="submit">{isEditing ? 'Update Flashcard' : 'Create Flashcard'}</Button>
+          <Button isPending={isPending} type="submit">
+            {isEditing ? 'Update' : 'Add'}
+          </Button>
         </div>
       </form>
     </Form>
