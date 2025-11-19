3dc2d3f5db8db2a755b70e870379fb8203b23373
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-19 21:46:12 +0700

    refactor: remove mutation hooks

--- src/modules/collection/hooks/collection.hooks.ts
+++ src/modules/collection/hooks/collection.hooks.ts
@@ -1,11 +1,5 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
-import {
-  createCollection,
-  getCollectionById,
-  getCollections,
-  updateCollection,
-} from '../services/collection.services';
-import type { FormCollectionType } from '../types/collection';
+import { useQuery } from '@tanstack/react-query';
+import { getCollectionById, getCollections } from '../services/collection.services';
 
 export const useGetCollections = () => {
   return useQuery({
@@ -18,17 +12,5 @@ export const useGetCollectionById = (id: number) => {
     queryKey: ['collections', id],
     queryFn: () => getCollectionById(id),
     enabled: !!id,
-  });
-};
-
-export const useCreateCollection = () => {
-  return useMutation({
-    mutationFn: (payload: FormCollectionType) => createCollection(payload),
-  });
-};
-
-export const useUpdateCollection = () => {
-  return useMutation({
-    mutationFn: (payload: FormCollectionType) => updateCollection(payload),
   });
 };
