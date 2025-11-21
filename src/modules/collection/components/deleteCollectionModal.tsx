b2654950664ab3c6141709e1c7c580a49fb83b9a
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-21 11:05:23 +0700

    feat: delete modal

---
+++ src/modules/collection/components/deleteCollectionModal.tsx
@@ -0,0 +1,39 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import React from 'react';
+
+const DeleteCollectionModal = ({
+  deleteId,
+  setDeleteId,
+}: {
+  deleteId: number | null;
+  setDeleteId: (value: number | null) => void;
+}) => {
+  return (
+    <AlertDialog open={!!deleteId} onOpenChange={() => setDeleteId(null)}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This will permanently delete this collection and all its flashcards. This action cannot
+            be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteCollectionModal;
