d7f863989dbdbc325110ba51a18c05d0bb899677
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-22 21:34:56 +0700

    feat: add text to speech by term

--- src/modules/flashcard/components/flashcard.tsx
+++ src/modules/flashcard/components/flashcard.tsx
@@ -2,9 +2,10 @@ import { Card } from '@/components/ui/card';
 import React from 'react';
 import type { FlashcardType } from '../types/flashcard';
 import { Button } from '@/components/ui/button';
-import { Edit, Trash2 } from 'lucide-react';
+import { Edit, Trash2, Volume2Icon } from 'lucide-react';
 import DeleteFlashcardModal from './deleteFlashcardModal';
 import EditFlashcardModal from './editFlashcardModal';
+import { textToSpeech } from '@/shared/utils/textToSpeech';
 
 const Flashcard = ({ card }: { card: FlashcardType }) => {
   const [openEdit, setOpenEdit] = React.useState(false);
@@ -12,19 +13,24 @@ const Flashcard = ({ card }: { card: FlashcardType }) => {
 
   return (
     <>
-      <Card className="p-4 hover:shadow-lg transition-shadow">
+      <Card className="p-6 hover:shadow-lg transition-shadow">
         <div className="space-y-3">
           <div className="flex items-start justify-between gap-2">
             <div className="flex-1 min-w-0">
-              <div className="font-semibold text-foreground truncate">{card.term}</div>
-              <div className="text-sm text-muted-foreground truncate">{card.definition}</div>
+              <div className="font-semibold text-lg text-foreground truncate">{card.term}</div>
+              <div className="text-[16px] text-muted-foreground truncate">{card.definition}</div>
+            </div>
+            <div className="">
+              <button className=" hover:cursor-pointer" onClick={() => textToSpeech(card.term)}>
+                <Volume2Icon className="size-5 text-muted-foreground hover:text-black/80" />
+              </button>
             </div>
           </div>
 
           <div className="flex gap-2 pt-2 border-t border-border">
             <Button
               variant="outline"
-              size="sm"
+              size="lg"
               className="flex-1"
               onClick={() => setOpenEdit(true)}
             >
@@ -33,8 +39,8 @@ const Flashcard = ({ card }: { card: FlashcardType }) => {
             </Button>
             <Button
               variant="outline"
-              size="sm"
-              className="flex-1 text-destructive hover:text-destructive hover:bg-destructive/10"
+              size="lg"
+              className="flex-1 text-destructive hover:text-destructive  hover:bg-destructive/10"
               onClick={() => setOpenDelete(card.id)}
             >
               <Trash2 className="w-4 h-4 mr-1" />
