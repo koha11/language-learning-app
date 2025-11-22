7c484fdbbc7416f2f9bf13d288be05c637f36ee2
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-22 21:33:48 +0700

    feat: add skeleton loading and navigate quiz page handler

--- src/modules/collection/pages/collectionDetail.tsx
+++ src/modules/collection/pages/collectionDetail.tsx
@@ -7,6 +7,8 @@ import FlashcardList from '@/modules/flashcard/components/flashcardList';
 import { useGetCollectionById } from '../hooks/collection.hooks';
 import AddFlashcardModal from '@/modules/flashcard/components/addFlashcardModal';
 import FlashcardSkeleton from '@/modules/flashcard/components/flashcardSkeleton';
+import CollectionHeaderSkeleton from '../components/collectionHeaderSkeleton';
+import FlashcardPraciceSkeleton from '@/modules/flashcard/components/flashcardPracticeSkeleton';
 
 const CollectionDetail = () => {
   const { id } = useParams();
@@ -14,7 +16,7 @@ const CollectionDetail = () => {
 
   const [addCard, setAddCard] = useState(false);
 
-  const { data, isLoading, isError } = useGetCollectionById(id!);
+  const { data, isLoading, isError } = useGetCollectionById(Number(id!));
 
   return (
     <div className="min-h-screen bg-background">
@@ -25,54 +27,67 @@ const CollectionDetail = () => {
               <ArrowLeft className="w-4 h-4 mr-2" />
               Back to Collections
             </Button>
-            <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
-              <div>
-                <h1 className="text-3xl font-bold text-foreground">{data?.name}</h1>
-                <p className="text-muted-foreground mt-1">{data?.description}</p>
-                <div className="flex flex-wrap gap-2 mt-3">
-                  {data?.tags ? (
-                    data.tags.split(' ').map((tag) => (
-                      <span
-                        key={tag}
-                        className="px-3 py-1 bg-primary/10 text-primary rounded-full text-sm"
-                      >
-                        {tag}
-                      </span>
-                    ))
-                  ) : (
-                    <span className="text-xs">No tags</span>
-                  )}
+            {isLoading ? (
+              <>
+                <CollectionHeaderSkeleton />
+                <div className="my-6">
+                  <FlashcardPraciceSkeleton />
                 </div>
-              </div>
-              <div className="flex gap-2">
-                <Link to={`/collections/${id}/quiz`} state={{ flashcards: data?.flashcards }}>
-                  <Button size="lg">
-                    <Play className="w-5 h-5 mr-2" />
-                    Start Quiz
-                  </Button>
-                </Link>
-                <Button onClick={() => setAddCard(true)} size="lg">
-                  <Plus className="w-5 h-5 mr-2" />
-                  Add Card
-                </Button>
-                <Button variant="outline" size="lg">
-                  <ShareIcon className="w-5 h-5" />
-                </Button>
-              </div>
-            </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                  {Array.from({ length: 12 }).map((_, i) => (
+                    <FlashcardSkeleton key={i} />
+                  ))}
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
+                  <div>
+                    <h1 className="text-3xl font-bold text-foreground">{data?.name}</h1>
+                    <p className="text-muted-foreground mt-1">{data?.description}</p>
+                    <div className="flex flex-wrap gap-2 mt-3">
+                      {data?.tags ? (
+                        data.tags.split(' ').map((tag) => (
+                          <span
+                            key={tag}
+                            className="px-3 py-1 bg-primary/10 text-primary rounded-full text-sm"
+                          >
+                            {tag}
+                          </span>
+                        ))
+                      ) : (
+                        <span className="text-xs">No tags</span>
+                      )}
+                    </div>
+                  </div>
+                  <div className="flex gap-2">
+                    <Button
+                      size="lg"
+                      onClick={() => {
+                        localStorage.removeItem(`quiz-progress-${id}`);
+                        navigate(`/collections/${id}/quiz`);
+                      }}
+                    >
+                      <Play className="w-5 h-5 mr-2" />
+                      Start Quiz
+                    </Button>
+
+                    <Button onClick={() => setAddCard(true)} size="lg">
+                      <Plus className="w-5 h-5 mr-2" />
+                      Add Card
+                    </Button>
+                    <Button variant="outline" size="lg">
+                      <ShareIcon className="w-5 h-5" />
+                    </Button>
+                  </div>
+                </div>
+                <div className="my-6">
+                  <FlashcardPractice flashcards={data?.flashcards ?? []} />
+                </div>
+                <FlashcardList flashcards={data?.flashcards ?? []} />
+              </>
+            )}
           </div>
-
-          <FlashcardPractice flashcards={data?.flashcards ?? []} />
-
-          {isLoading ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {Array.from({ length: 12 }).map((_, i) => (
-                <FlashcardSkeleton key={i} />
-              ))}
-            </div>
-          ) : (
-            <FlashcardList flashcards={data?.flashcards ?? []} />
-          )}
         </div>
       </div>
       <AddFlashcardModal open={addCard} onChange={() => setAddCard(false)} />
