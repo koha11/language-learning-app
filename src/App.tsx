12fb02e3065d11b3595e1a1fcbbe91270f126034
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-11-22 21:30:02 +0700

    feat: add config refetch in react query

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import { Toaster } from './components/ui/sonner';
 import { router } from './core/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
